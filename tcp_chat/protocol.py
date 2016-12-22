8392b0976ae430cc6adc86735999a44528204aac
rvsosn <rvsosnovsky@gmail.com>
2016-12-22 13:05:13 +0300

    Change protocol

--- tcp_chat/protocol.py
+++ tcp_chat/protocol.py
@@ -1,35 +1,16 @@
-BUFFER_SIZE = 1024
-SHIELDING_BYTE = 0x25
-STOP_BYTE = 0x26
+import struct
 
+LEN_STRUCT = struct.Struct('<I') # unsigned int
 
 def recv_until_end_messages(sock):
-    data = bytearray()
-    finish_receiving = False
+    received_data_len = LEN_STRUCT.unpack(sock.recv(LEN_STRUCT.size))[0]
+    received_data = b''
 
-    while not finish_receiving:
-        received_data = sock.recv(BUFFER_SIZE)
+    while len(received_data) < received_data_len:
+        received_data += sock.recv(received_data_len-len(received_data))
 
-        if not received_data: break
-
-        is_shielded = False
-        for i in received_data:
-            if not is_shielded and i == SHIELDING_BYTE:
-                is_shielded = True
-            elif not is_shielded and i == STOP_BYTE:
-                finish_receiving = True
-                break
-            else:
-                is_shielded = False
-                data.append(i)
-    return bytes(data)
+    return received_data
 
 
 def send_message(sock, data):
-    prepared_data = bytearray()
-    for i in data:
-        if i == SHIELDING_BYTE or i == STOP_BYTE:
-            prepared_data.append(SHIELDING_BYTE)
-        prepared_data.append(i)
-    prepared_data.append(STOP_BYTE)
-    sock.send(prepared_data)
+    sock.send(LEN_STRUCT.pack(len(data)) + data)
